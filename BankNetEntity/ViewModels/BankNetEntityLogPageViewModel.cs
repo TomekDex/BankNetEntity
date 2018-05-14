77096a7d408fd7e212c18e00f688d6c8df4f9765
Tomasz Juszczak <tjtj@poczta.onet.pl>
2018-05-14 22:05:11 +0200

    Przykłądowe bindowanie

--- BankNetEntity/ViewModels/BankNetEntityLogPageViewModel.cs
+++ BankNetEntity/ViewModels/BankNetEntityLogPageViewModel.cs
@@ -10,6 +10,25 @@ namespace BankNetEntity.ViewModels
 {
     public class BankNetEntityLogPageViewModel : Screen
     {
-        
+        string login = "twój login";
+        public string Login
+        {
+            get
+            {
+                return login;
+            }
+
+            set
+            {
+                login = value;
+                NotifyOfPropertyChange(() => Login);
+            }
+        }
+
+        public void LogIn()
+        {
+            Login = "zalogowałeś się";
+
+        }
     }
 }
